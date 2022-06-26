7516a8ba3f95ff7ead55b024c2ef62d6608f3454
Cesar Romero Villarreal <89871050+cesarwizeline@users.noreply.github.com>
2022-06-26 00:07:39 -0500

    scss to css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 // styles
-import 'bootstrap/scss/bootstrap.scss';
+// import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'assets/scss/paper-kit.scss?v=1.3.0';
 import 'assets/demo/demo.css?v=1.3.0';
 import Index from 'views/Index.js';
-import NucleoIcons from 'views/NucleoIcons.js';
+// import NucleoIcons from 'views/NucleoIcons.js';
 import LandingPage from 'views/examples/LandingPage.js';
 import ProfilePage from 'views/examples/ProfilePage.js';
 import RegisterPage from 'views/examples/RegisterPage.js';
@@ -17,10 +18,10 @@ ReactDOM.render(
 	<BrowserRouter>
 		<Switch>
 			<Route path="/index" render={(props) => <Index {...props} />} />
-			<Route
+			{/* <Route
 				path="/nucleo-icons"
 				render={(props) => <NucleoIcons {...props} />}
-			/>
+			/> */}
 			<Route
 				path="/landing-page"
 				render={(props) => <LandingPage {...props} />}
